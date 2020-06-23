64420ea8c0c46cf06bb8b3bd07e7966f2f4ab508
nooruli <saytonoor@gmail.com>
2020-06-23 05:18:37 +0000

    2

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var routes=require('./routes/Myroutes.js');
 var rot=require('./routes/yo.js');
 var app=express();
 
+app.set('port',(process.env.PORT||5000));
+//app.use(express.static(_dirname+'/models'));
+
 app.use('/',routes);
 app.use('/yo',rot);
 
-var server =app.listen(4000,function(){});
+app.listen(app.get('port'),function(){
+  console.log("Node app is running 0n "+app.get('port'))
+});
