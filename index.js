7fc9d5c65361456bad1b2978ec5f1790a1609983
nooruli <saytonoor@gmail.com>
2020-06-23 06:51:31 +0000

    7

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var routes=require('./routes/Myroutes.js');
 var rot=require('./routes/yo.js');
 var app=express();
 
-app.set('port',(process.env.PORT||5000));
+app.set('port',(process.env.PORT||4000));
 //app.use(express.static(_dirname+'/models'));
 
 app.use('/',routes);
