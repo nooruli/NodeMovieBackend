bd19d2ced4fe4abc990eb9ba8e5d1162a2cf2b26
nooruli <47549783+nooruli@users.noreply.github.com>
2020-06-26 20:15:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express=require('express');
-var routes=require('./routes/Myroutes.js');
-var rot=require('./routes/yo.js');
+var cors = require('cors')
+
+var trending=require('./routes/trending.js');
+var search=require('./routes/search.js');
+
 var app=express();
+app.use(cors());
 
 app.set('port',(process.env.PORT||4000));
 //app.use(express.static(_dirname+'/models'));
 
-app.use('/',routes);
-app.use('/yo',rot);
+app.use('/',trending);
+app.use('/search',search);
 
 app.listen(app.get('port'),function(){
   console.log("Node app is running 0n "+app.get('port'))
