da6cf143f693314781a719a1190a3ffd1e5fa21f
nooruli <47549783+nooruli@users.noreply.github.com>
2020-06-29 13:59:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express=require('express');
-var cors = require('cors')
+var cors = require('cors');
 
 var trendingRoute=require('./routes/trending.js');
 var searchRoute=require('./routes/search.js');
