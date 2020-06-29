59c0d1e0df727c73818046f8cef373a5a1d5f981
nooruli <47549783+nooruli@users.noreply.github.com>
2020-06-29 13:25:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express=require('express');
 var cors = require('cors')
 
-var trending=require('./routes/trending.js');
-var search=require('./routes/search.js');
+var trendingRoute=require('./routes/trending.js');
+var searchRoute=require('./routes/search.js');
+var getfavRoute=require('./routes/getfav.js');
+var addfavRoute=require('./routes/addfav.js');
 
 var app=express();
 app.use(cors());
@@ -10,8 +12,10 @@ app.use(cors());
 app.set('port',(process.env.PORT||4000));
 //app.use(express.static(_dirname+'/models'));
 
-app.use('/',trending);
-app.use('/search',search);
+app.use('/',trendingRoute);
+app.use('/search',searchRoute);
+app.use('/getfav',getfavRoute);
+app.use('/addfav',addfavRoute);
 
 app.listen(app.get('port'),function(){
   console.log("Node app is running 0n "+app.get('port'))
