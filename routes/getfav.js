bf6c9628804aec5e676c1fc500c1f0d7ad5002b3
nooruli <47549783+nooruli@users.noreply.github.com>
2020-06-30 12:56:04 +0530

    Update getfav.js

--- routes/getfav.js
+++ routes/getfav.js
@@ -1,23 +1,28 @@
-var pg = require('pg');
+const { Client } = require('pg');
 var express=require('express');
 var router = express.Router();
 
-var conString = "postgres://ebobuoul:uz8TZ_oQueJ768MnuGmUoxWtN5Lv_ws9@ruby.db.elephantsql.com:5432/ebobuoul" //Can be found in the Details page
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT * from movie', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    
-    router.get("/",(req,res)=>{res.send(result.rows)});
+router.get('/',(req,res)=>{
    
-    client.end();
-  });
+var DATABASE_URL="postgres://ubvrsggnkyomah:a6d43665456f3d123013c69c878738e8edead869a4b26356baa432a01e013931@ec2-35-153-12-59.compute-1.amazonaws.com:5432/d66jmdqke3ae0h";
+
+const client = new Client({
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
+client.connect();
+
+client.query(`SELECT * from movie`, (err, result) => {
+  if (err) throw err;
+  for (let row of result.rows) {
+    console.log(JSON.stringify(row));
+  }
+  res.json(result.rows);
+  client.end();
+});
+ });
 
 module.exports=router;
